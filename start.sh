56fe7ba1f7750de700c39bd0bd99e04df404dde2
vincent-tarrit <91567748+vincent-tarrit@users.noreply.github.com>
2026-04-15 14:46:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,40 +16,37 @@ fi
 usermod -aG docker docker
 chown -R docker:docker /home/docker
 
-# ---- EXPORT ENV FOR CHILD ----
-export REPO
-export REG_TOKEN
-export NAME
-
 echo "Switching to docker user..."
 
-# ---- RUN AS DOCKER USER ----
-exec su - docker -c '
-set -e
+# ---- PASS ENV VARIABLES EXPLICITLY ----
+exec su - docker -c "
+export REPO='${REPO}'
+export REG_TOKEN='${REG_TOKEN}'
+export NAME='${NAME}'
 
-echo "Running as: $(whoami)"
-echo "Repo: $REPO"
+echo 'Running as:' \$(whoami)
+echo 'Repo:' \$REPO
 
 cd /home/docker/actions-runner || exit
 
 if [ ! -f .runner ]; then
-  echo "Configuring runner..."
+  echo 'Configuring runner...'
   ./config.sh \
-    --url https://github.com/${REPO} \
-    --token ${REG_TOKEN} \
-    --name ${NAME}-$(hostname) \
+    --url https://github.com/\${REPO} \
+    --token \${REG_TOKEN} \
+    --name \${NAME}-\$(hostname) \
     --unattended \
     --replace
 fi
 
 cleanup() {
-  echo "Removing runner..."
-  ./config.sh remove --unattended --token ${REG_TOKEN}
+  echo 'Removing runner...'
+  ./config.sh remove --unattended --token \${REG_TOKEN}
 }
 
-trap "cleanup; exit 130" INT
-trap "cleanup; exit 143" TERM
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
 
-echo "Starting runner..."
+echo 'Starting runner...'
 exec ./run.sh
-'
+"
