9af056cde30fa3038f7b3e2f424a0bba718b73bb
vincent-tarrit <91567748+vincent-tarrit@users.noreply.github.com>
2026-04-15 14:20:10 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-# Fix docker socket permissions dynamically
-DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
-
-echo "Docker socket GID: $DOCKER_GID"
-
-# Create or update docker group to match socket
-if getent group docker >/dev/null; then
-  echo "Updating docker group GID"
-  groupmod -g "$DOCKER_GID" docker
-else
-  echo "Creating docker group"
-  groupadd -g "$DOCKER_GID" docker
-fi
-
-# Add user to docker group
-usermod -aG docker docker
-
-# Refresh group membership
-newgrp docker <<EOF
-echo "Docker group applied"
-EOF
-
 REPO=$REPO
 REG_TOKEN=$REG_TOKEN
 NAME=$NAME
@@ -37,4 +15,10 @@ cleanup() {
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
-./run.sh & wait $!
+# fix permissions as root
+DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
+groupmod -g "$DOCKER_GID" docker || true
+usermod -aG docker docker
+
+# switch user
+exec sudo -u docker /home/docker/actions-runner/run.sh
